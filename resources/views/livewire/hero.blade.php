1cbe60430c00adc84775bfc2b28feb378ef85bf4
Alfian Dorif Murtadlo1234 <100700571+IndyJones123@users.noreply.github.com>
2024-08-08 00:26:24 +0700

    Welcome Home & Navbar Livewire Finish

---
+++ resources/views/livewire/hero.blade.php
@@ -0,0 +1,19 @@
+<div class="flex flex-col lg:flex-row gap-5 max-w-full mx-auto overflow-x-hidden">
+    <div class="lg:flex-none lg:w-1/2 flex flex-col justify-center items-center max-w-full p-3 lg:gap-10 lg:items-start lg:ml-10">
+        <div class="text-2xl text-black max-w-full text-center lg:text-start lg:text-7xl 2xl:text-8xl">
+            <h1>Media buku terbaik</h1>
+            <h2 class="text-red-400">untuk penulis</h2>
+        </div>
+        <div class="flex flex-row gap-3 text-lg text-black max-w-full mt-3">
+            @auth
+            <a class="font-semibold text-white bg-yellow-500 hover:bg-yellow-600 dark:bg-yellow-700 dark:hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 px-4 py-2 rounded" href="{{route('filament.user.pages.dashboard')}}">GET STARTED</a>
+            @else
+            <a class="font-semibold text-white bg-yellow-500 hover:bg-yellow-600 dark:bg-yellow-700 dark:hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 px-4 py-2 rounded" href="{{route('filament.user.auth.login')}}">GET STARTED</a>
+            @endauth
+            <a class="font-semibold text-white bg-yellow-500 hover:bg-yellow-600 dark:bg-yellow-700 dark:hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 px-4 py-2 rounded" href="">BROWSE BOOKS</a>
+        </div>
+    </div>
+    <div class="lg:flex-none lg:w-1/2 flex items-center justify-center max-w-full p-3">
+        <img class="w-3/4 lg:w-7/12 lg:h-full mx-auto rounded-lg mt-3 max-w-full" src="https://images.unsplash.com/photo-1529158062015-cad636e205a0?q=80&w=1353&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="">
+    </div>
+</div>
---
+++ resources/views/livewire/navigation.blade.php
@@ -0,0 +1,66 @@
+<div class="flex flex-row bg-dots-darker bg-center bg-white dark:bg-dots-lighter  selection:bg-red-500 selection:text-white">
+    <div class="flex-none w-14 m-3 p-3">
+        <img class="rounded-lg" src="{{ asset('/favicon.ico') }}" alt="Logo">
+    </div>
+    <div class="flex-none w-36 m-3 p-3 text-black">
+        <a href="{{route("home")}}">PT FAN BOOKS</a>
+    </div>
+    <div class="flex-none w-36 m-3 p-3 text-black">
+        <a href="">COLLECTION</a>
+    </div>
+    <div class="flex-1 m-3 p-3">
+        <div class="flex justify-end items-end text-right">
+            @if (Route::has('filament.user.pages.dashboard'))
+                {{-- PC --}}
+                <div class="hidden lg:block">
+                    @auth
+                        <a href="{{ route('filament.user.pages.dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                            Dashboard
+                        </a>
+                    @else
+                        <a href="{{ route('filament.user.auth.login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                            LOGIN
+                        </a>
+                        <a href="{{ route('filament.user.auth.register') }}" class="ml-4 font-semibold text-white bg-yellow-500 hover:bg-yellow-600 dark:bg-yellow-700 dark:hover:bg-yellow-800 focus:outline-none focus:ring-2 focus:ring-yellow-500 focus:ring-offset-2 px-4 py-2 rounded">
+                            SIGN UP FOR FREE
+                        </a>
+                    @endauth
+                </div>
+
+                {{-- Mobile --}}
+                <div class="lg:hidden relative">
+                    <button id="menuButton" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10" viewBox="0 0 20 20" fill="currentColor">
+                            <path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path>
+                        </svg>
+                    </button>
+                    <div id="menuItems" class="absolute right-0 mt-2 hidden bg-white shadow-lg dark:bg-gray-800">
+                        @auth
+                            <a href="{{ route('filament.user.pages.dashboard') }}" class="block px-4 py-2 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                                Dashboard
+                            </a>
+                        @else
+                            <a href="{{ route('filament.user.auth.login') }}" class="block px-4 py-2 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                                LOGIN
+                            </a>
+                            <a href="{{ route('filament.user.auth.register') }}" class="block px-4 py-2 text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">
+                                REGISTER
+                            </a>
+                        @endauth
+                    </div>
+                </div>
+            @endif
+        </div>
+    </div>
+</div>
+
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+        const menuButton = document.getElementById('menuButton');
+        const menuItems = document.getElementById('menuItems');
+
+        menuButton.addEventListener('click', function() {
+            menuItems.classList.toggle('hidden');
+        });
+    });
+</script>
